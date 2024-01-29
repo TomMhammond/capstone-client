cf5dfae3a6b7fdd3dfc869bd91c3c06ca3f40cf4
Thomas Hammond <tomhammond32@gmail.com>
2024-01-29 17:27:13 -0500

    add clickhandler to Client component to navigate to single client page

--- src/components/Client/Client.jsx
+++ src/components/Client/Client.jsx
@@ -1,8 +1,15 @@
 import './Client.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function Client({ client }){
+    const navigate = useNavigate();
+    
+    const clickHandler = (e) => {
+        navigate(`/clients/${client.id}`)
+    }
+    
     return(
-        <div className='client'>
+        <div className='client' onClick={clickHandler}>
             <p className='client__first-name'>{client.first_name}</p>
             <p className='client__last-name'>{client.last_name}</p>
             <p className='client__phone'>{client.phone}</p>
