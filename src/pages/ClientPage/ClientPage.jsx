679a563b1b8295457cb7c4b2ebf92d9e23836408
Thomas Hammond <tomhammond32@gmail.com>
2024-01-29 15:18:32 -0500

    add axios call to clients page

--- src/pages/ClientPage/ClientPage.jsx
+++ src/pages/ClientPage/ClientPage.jsx
@@ -1,6 +1,23 @@
-import './ClientPage.scss'
+import './ClientPage.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function ClientPage(){
+    const [clientList, setCLientList ] = useState([]);
+    useEffect(() => {
+        const fetchClientList = async () => {
+            const response = await axios.get('http://localhost:8080/clients');
+            setCLientList(response.data)
+        } 
+        fetchClientList();
+    }, []);
+
+    if(clientList.length === 0){
+        return <></>
+    };
+
+    console.log(clientList)
+
     return(
         <></>
     )
