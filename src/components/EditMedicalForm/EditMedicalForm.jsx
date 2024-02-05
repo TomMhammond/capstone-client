204ca05bc5d5d7b11e7d40ca7f58adf99b83d7ba
Thomas Hammond <tomhammond32@gmail.com>
2024-02-05 10:51:46 -0500

    complete edit medical

--- src/components/EditMedicalForm/EditMedicalForm.jsx
+++ src/components/EditMedicalForm/EditMedicalForm.jsx
@@ -1,15 +1,126 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import '../MedicalForm/MedicalForm.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-export default function EditMedicalForm(){
+export default function EditMedicalForm({ token }){
     const navigate = useNavigate();
+    const params = useParams();
+    const id = params.id;
+    const [ medical, setMedical ] =useState(null);
+
+    useEffect(() => {
+        const fetchMedical = async () => {
+            const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/medical/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            setMedical(response.data[0]);
+        }
+        fetchMedical()
+    }, [])
+
+    if(!medical){
+        return <></>
+    }
+
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        const formEl = e.target;
+        const allergy1El = formEl.allergy1.value;
+        let allergy1;
+        if(!allergy1El){
+            allergy1 = medical.allergy1;
+        } else {
+            allergy1 = allergy1El;
+        }
+        
+        const allergy2El = formEl.allergy2.value;
+        let allergy2;
+        if(!allergy2El){
+            allergy2 = medical.allergy2;
+        } else {
+            allergy2 = allergy2El;
+        }
+
+        const allergy3El = formEl.allergy3.value;
+        let allergy3;
+        if(!allergy3El){
+            allergy3 = medical.allergy3;
+        } else {
+            allergy3 = allergy3El;
+        }
+
+        const condition1El = formEl.condition1.value;
+        let condition1;
+        if(!condition1El){
+            condition1 = medical.condition1;
+        } else {
+            condition1 = condition1El;
+        }
+
+        const condition2El = formEl.condition2.value;
+        let condition2;
+        if(!condition2El){
+            condition2 = medical.condition2;
+        } else {
+            condition2 = condition2El;
+        }
+
+        const condition3El = formEl.condition3.value;
+        let condition3;
+        if(!condition3El){
+            condition3 = medical.condition3;
+        } else {
+            condition3 = condition3El;
+        }
+
+        const neuterEl = formEl.neuter.value;
+        let neuter;
+        if(!neuterEl && neuterEl !== 0){
+            neuter = Number(medical.is_neutured);
+        } else {
+            neuter = Number(neuterEl);
+        }
+
+        const vaccinatedEl = formEl.vaccinated.value;
+        let vaccinated;
+        if(!vaccinatedEl && vaccinatedEl !== 0){
+            vaccinated = Number(medical.is_vaccinated);
+        } else {
+            vaccinated = Number(vaccinatedEl);
+        }
+
+        const body = 
+        {
+            pet_id: id,
+            is_neutured: neuter,
+            is_vaccinated: vaccinated,
+            allergy1,
+            allergy2,
+            allergy3,
+            condition1,
+            condition2,
+            condition3
+        }
+
+       await axios.put(`${process.env.REACT_APP_BASE_URL}/medical/${id}`, body, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+       });
+       navigate(`/pets/${id}`)
+    }
 
     const cancelClickHandler = (e) => {
         e.preventDefault();
         navigate('/pets')
     }
     return(
-        <form className='medical-form'>
+        <form className='medical-form' onSubmit={submitHandler}>
             <div className='medical-form__container'>
                 <h4 className='medical-form__subtitle'>Allergies</h4>
                 <div className='medical-form__wrapper'>
