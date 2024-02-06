cdcdca0d7ba4df7c860b35b1753297cb009faa7f
Thomas Hammond <tomhammond32@gmail.com>
2024-02-06 09:37:00 -0500

    add base inventory list

---
+++ src/components/Inventory/Inventory.jsx
@@ -0,0 +1,11 @@
+import './Inventory.scss';
+
+export default function Inventory({ inventory }){
+    return(
+        <div className='inventory'>
+            <p className='inventory__name'>{inventory.item}</p>
+            <p className='inventory__cat'>{inventory.category}</p>
+            <p className='inventory__onHand'>{inventory.amount_on_hand}</p>
+        </div>
+    )
+}
--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,14 +1,33 @@
 import './InventoryPage.scss';
 import SideNav from '../../components/SideNav/SideNav';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Inventory from '../../components/Inventory/Inventory';
 
 export default function InventoryPage({ access, token }){
+    const [ inventoryData, setInventoryData ] = useState([]);
+
+    useEffect(() => {
+        const fetchInventoryData = async () => {
+            const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/inventory`);
+            setInventoryData(response.data);
+        }
+        fetchInventoryData();
+    }, []);
+
+    if(!inventoryData){
+        return <></>
+    }
+    
     return(
         <main className='main'>
             <SideNav access={access}/>
             <section className='page'>
                 <div className='inventory-card'>
                     <h2 className='inventory-card__title'>Inventory</h2>
-
+                    {inventoryData.map((inventory) => (
+                        <Inventory key={inventory.id} inventory={inventory}/>
+                    ))}
                 </div>
             </section>
         </main>
