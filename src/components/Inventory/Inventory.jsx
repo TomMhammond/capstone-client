eb85b328a0f8df51b257fb617dbff23d288c7849
Thomas Hammond <tomhammond32@gmail.com>
2024-02-06 09:39:22 -0500

    add styling to inventory page

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -3,7 +3,7 @@ import './Inventory.scss';
 export default function Inventory({ inventory }){
     return(
         <div className='inventory'>
-            <p className='inventory__name'>{inventory.item}</p>
+            <p className='inventory__item'>{inventory.item}</p>
             <p className='inventory__cat'>{inventory.category}</p>
             <p className='inventory__onHand'>{inventory.amount_on_hand}</p>
         </div>
