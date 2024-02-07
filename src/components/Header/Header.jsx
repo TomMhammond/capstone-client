82d12cefe3cd3a2f1b755c4e26376d2c88bb8503
Thomas Hammond <tomhammond32@gmail.com>
2024-02-07 10:35:30 -0500

    add image to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import './Header.scss';
+import image from '../../assets/images/paw-prints.jpeg'
 
 export default function Header({ isLoggedIn, userName, setIsLoggedIn, setUserName, setAccess }){
     const navigate = useNavigate();
@@ -25,7 +26,8 @@ export default function Header({ isLoggedIn, userName, setIsLoggedIn, setUserNam
 
     return(
         <header className='header'>
-            <h2 className='header__title' onClick={clickHandler}>Pawsome Practice Manager</h2>
+                <img className='header__img' src={image}></img>
+                <h2 className='header__title' onClick={clickHandler}>Pawsome Practice Manager</h2>
             <ul className='header__list'>
                 <li className='header__list-item header__list-item--left'>{text}</li>
                 <li className='header__list-item--left' onClick={logOutClickHandler}>{logOut}</li>
